673fd3e9bbe262c13d1137dedb5dc5fadea4b7d0
michelsonhnsn <michelson@poucedigital.com>
2021-07-21 03:22:54 +0200

    Create CheckBox.vue

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="" v-for="option in options" :key="option.id">
+      <input type="checkbox" :name="option.name" :value="option.value">
+          {{option.text}}
+       <slot></slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      options: [
+        { text: 'Cinéma', value: 'movie', name: 'interest' },
+        { text: 'Sports', value: 'sport', name: 'interest'  },
+        { text: 'Voyage', value: 'travel', name: 'interest'  },
+        { text: 'Théâtre', value: 'theater', name: 'interest'  },
+        { text: 'Concert', value: 'concert', name: 'interest' }
+    ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+label {
+  display: block;
+  text-align: left !important;
+}
+</style>
